fd7ec3781a6cb2c1367e1de36c657172ce51eb32
Nawab Surya <suryaprakash8707yadav@gmail.com>
2026-02-11 09:59:32 +0530

    Modified index.js

--- day_04/index.js
+++ day_04/index.js
@@ -1,22 +1,21 @@
-// import {readFile} from "./readANDWrite.js";
-// const fileData = async(path)=>{
-//     try{
-//         const data = await readFile(path);
-//         console.log(data);
-//     } catch (error){
-//         console.log("unable to read");
-//     }
-// }
-// export const writeFile = async (path, data) => {
-//     try{
-//         await fs.writeFile (path, data);
-//     } catch (error){
-//         console.log("unable to find data")
-//     }
-// }
-// writeData("./students.json",`{id:4, name:"D"}`);
+import {readFile} from "./readANDWrite.js";
+const fileData = async(path)=>{
+    try{
+        const data = await readFile(path);
+        console.log(data);
+    } catch (error){
+        console.log("unable to read");
+    }
+}
+export const writeFile = async (path, data) => {
+    try{
+        await fs.writeFile (path, data);
+    } catch (error){
+        console.log("unable to find data")
+    }
+}
+writeData("./students.json",`{id:4, name:"D"}`);
 
-index.js
 
 import { readFile, writeFile } from "./readANDWrite.js";
 
