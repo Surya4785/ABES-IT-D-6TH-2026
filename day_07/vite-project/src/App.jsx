0849bb8796710395f15f8464a53f24285ae90317
Nawab Surya <suryaprakash8707yadav@gmail.com>
2026-03-18 10:06:24 +0530

    User List

--- day_07/vite-project/src/App.jsx
+++ day_07/vite-project/src/App.jsx
@@ -1,31 +1,31 @@
 import "./App.css";
-import { useState } from 'react';
-import axios from 'axios';
-const API = "http://localhost:8800/login";
-const App = () => {
-  const [userDetail, setUserDetail] = useState({
-    email: "",
-    password: ""
-  })
-  console.log(userDetail);
-  const changeHandler = (e) => {
-    const { name, value } = e.target;
-    setUserDetail((prev) => ({ ...prev,  [name]: value }))
-  }
-  const submitHandler = async() => {
-    const response = await axios.post(API,userDetail);
-    console.log(response);  
-  }
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div>
-      <h1>LOGIN</h1>
-      <br />
-      <input type="email" name="email" id="email" onChange={changeHandler} />
-      <br></br>
-      <input type="password" name="password" id="password" onChange={changeHandler} /><br />
-      <button onClick={submitHandler}>LOGIN</button>
+    <div className="container">
+      <h1 className="title">Users List</h1>
+
+      <div className="grid">
+        {data.map(user => (
+          <div key={user.id} className="card">
+            <h3>{user.name}</h3>
+            <p>{user.email}</p>
+            <span className="badge">{user.company.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
